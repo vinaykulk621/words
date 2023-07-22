2f5331248579991a033142c8eaf90965611d8c38
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-07-22 22:00:01 +0530

    shadcn && cmdk

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,20 @@
+import Cmdk from "./cmdk";
 import words from "./words";
 
 export default function Home() {
+  let gay: Array<string> = []
+  for (let i = 0; i <= 5000; i++) {
+    gay.push(words[Math.floor(Math.random() * words.length)]);
+  }
+
   return (
-    <div className="flex flex-wrap text-2xl font-semibold space-x-4 p-4">
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-      <span>Words</span>
-    </div>
+    <>
+      <div className="flex flex-wrap space-x-3 h-fit items-center justify-center bg-black text-white">
+        {gay.map(e => {
+          return <span key={Math.random() * 68524578}>{e}</span>
+        })}
+        <Cmdk />
+      </div >
+    </>
   )
 }
