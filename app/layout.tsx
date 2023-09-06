79841c547d7d713b876cb400bfb6eef0b4b892f0
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-09-06 12:14:10 +0530

    OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,21 @@ export const metadata: Metadata = {
     'Responsive web design with Next.js and Tailwind CSS',
     'Fast-loading website using Next.js and Supabase database',
   ],
+  openGraph: {
+    images: ['https://words-livid.vercel.app/og.jpg'],
+    description: 'Infinite scroll feature for seamless content browsing',
+    type: 'website',
+    url: 'https://words-livid.vercel.app',
+    title: 'words-livid',
+  },
+  twitter: {
+    images: ['https://words-livid.vercel.app/og.jpg'],
+    description: 'Infinite scroll feature for seamless content browsing',
+    card: 'summary_large_image',
+    site: 'https://words-livid.vercel.app',
+    title: 'Infinite scroll feature in Next js 13 using server actions.',
+    creator: 'Jackx',
+  },
 }
 
 export default function RootLayout({
